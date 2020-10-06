e51eb085a44d764aa3381a218113670c7a13ea38
renonakamoto <obrsk23@yahoo.co.jp>
2020-10-06 13:07:40 +0900

    駒のを十字キーで入力できるように変更

--- MicroShogi/MicroShogi/MicroShogi/ShogiObject/ShogiPlayer/PC/ShogiPlayerPC.cpp
+++ MicroShogi/MicroShogi/MicroShogi/ShogiObject/ShogiPlayer/PC/ShogiPlayerPC.cpp
@@ -7,33 +7,34 @@
 /*=========================*/
 bool ShogiPlayerPC::SelectSource()
 {
+	printf("十字キーで選択\n");
 	printf("【移動元の入力】\n");
 
-	while (true)
-	{
-		printf("横軸を入力してください\n");
-		
-		int key = Input::GetKey();
-
-		if (key == 'a' || key == 'A')      { m_MoveSource.m_X = 0; break; }
-		else if (key == 'b' || key == 'B') { m_MoveSource.m_X = 1; break; }
-		else if (key == 'c' || key == 'C') { m_MoveSource.m_X = 2; break; }
-		else if (key == 'd' || key == 'D') { m_MoveSource.m_X = 3; break; }
-	}
+	IVec2 select_unit(0, 0);
 
 	while (true)
 	{
-		printf("縦軸を入力してください\n");
-
+		COORD cursor_pos{ select_unit.m_X * 4 + 4, select_unit.m_Y * 2 + 2 };
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursor_pos);
 		int key = Input::GetKey();
 
-		if (key == '1')      {m_MoveSource.m_Y = 0;	break; }
-		else if (key == '2') {m_MoveSource.m_Y = 1;	break; }
-		else if (key == '3') {m_MoveSource.m_Y = 2;	break; }
-		else if (key == '4') {m_MoveSource.m_Y = 3;	break; }
-		else if (key == '5') {m_MoveSource.m_Y = 4;	break; }
+		if (key == 77)      { select_unit.m_X++; }
+		else if (key == 75) { select_unit.m_X--; }
+		else if (key == 80) { select_unit.m_Y++; }
+		else if (key == 72) { select_unit.m_Y--; }
+		else if (key == 32) {
+			m_MoveSource.m_X = select_unit.m_X;
+			m_MoveSource.m_Y = select_unit.m_Y;
+			break;
+		}
+
+		if (select_unit.m_X < 0) select_unit.m_X = 0;
+		if (select_unit.m_X > 3) select_unit.m_X = 3;
+		if (select_unit.m_Y < 0) select_unit.m_Y = 0;
+		if (select_unit.m_Y > 4) select_unit.m_Y = 4;
 	}
 
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,BOARD_FRAME_HEIGHT + 1 });
 	return m_RefBoard->IsAbleSelectPiece(m_MoveSource, m_Id);
 }
 
@@ -43,33 +44,37 @@ bool ShogiPlayerPC::SelectSource()
 /*=========================*/
 bool ShogiPlayerPC::SelectDest()
 {
+	printf("十字キーで選択\n");
+	printf("移動元->横[%c],縦[%c]\n", 'A' + m_MoveSource.m_X, '1' + m_MoveSource.m_Y);
 	printf("【移動先の入力】\n");
-	IVec2 move_dest{ 0,0 };
+	IVec2 select_unit{ 0,0 };
 
 	while (true)
 	{
-		printf("横軸を入力してください\n");
-
+		COORD cursor_pos{ select_unit.m_X * 4 + 4, select_unit.m_Y * 2 + 2 };
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursor_pos);
 		int key = Input::GetKey();
 
-		if (key == 'a' || key == 'A')      { move_dest.m_X = 0; break; }
-		else if (key == 'b' || key == 'B') { move_dest.m_X = 1; break; }
-		else if (key == 'c' || key == 'C') { move_dest.m_X = 2; break; }
-		else if (key == 'd' || key == 'D') { move_dest.m_X = 3; break; }
+		if (key == 77) { select_unit.m_X++; }
+		else if (key == 75) { select_unit.m_X--; }
+		else if (key == 80) { select_unit.m_Y++; }
+		else if (key == 72) { select_unit.m_Y--; }
+		else if (key == 32) {
+			break;
+		}
+
+		if (select_unit.m_X < 0) select_unit.m_X = 0;
+		if (select_unit.m_X > 3) select_unit.m_X = 3;
+		if (select_unit.m_Y < 0) select_unit.m_Y = 0;
+		if (select_unit.m_Y > 4) select_unit.m_Y = 4;
 	}
 
-	while (true)
-	{
-		printf("縦軸を入力してください\n");
 
-		int key = Input::GetKey();
-
-		if (key == '1')      { move_dest.m_Y = 0;	break; }
-		else if (key == '2') { move_dest.m_Y = 1;	break; }
-		else if (key == '3') { move_dest.m_Y = 2;	break; }
-		else if (key == '4') { move_dest.m_Y = 3;	break; }
-		else if (key == '5') { move_dest.m_Y = 4;	break; }
-	}
-
-	return m_RefBoard->IsAblePutOnTheBoard(m_MoveSource, move_dest, m_Id);
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,BOARD_FRAME_HEIGHT + 3 });
+	printf("\r                                    \r");
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,BOARD_FRAME_HEIGHT + 2 });
+	printf("\r                                    \r");
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,BOARD_FRAME_HEIGHT + 1 });
+	printf("\r                                    \r");
+	return m_RefBoard->IsAblePutOnTheBoard(m_MoveSource, select_unit, m_Id);
 }
