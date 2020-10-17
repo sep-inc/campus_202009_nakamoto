518968be96ab766628fd1d5384721e5f3c7dcf24
renonakamoto <obrsk23@yahoo.co.jp>
2020-10-18 00:14:59 +0900

    タイトルシーンの作成

---
+++ BlockoutOnUnity/Assets/Scripts/GameSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSystem : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void EndGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+}
